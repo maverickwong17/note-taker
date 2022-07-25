598df80b0ec94e0908a02a3492b6885399c4872f
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-07-25 01:42:53 -0700

    post requests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json')
+const fs = require('fs')
 
 const PORT = 3001;
 
@@ -11,8 +13,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+    const { title, text } = req.body;
+
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        // note_id: uuid(),
+      };
+
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+          parsedNotes.push(newNote);
+          fs.writeFile('./db/db.json',JSON.stringify(parsedNotes, null, 4),(writeErr) =>
+              writeErr ? console.error(writeErr) : console.info('Successfully updated reviews!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.json(response);
+    } else {
+      res.json('Error in posting notes');
+    }
+  });
+  
 
 app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
