408b9e9c0f104c910c8cc3ef93a7d98985a1bcff
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-07-25 23:36:16 -0700

    update port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const notes = require('./db/notes.json')
 const fs = require('fs')
 const uuid = require('./helpers/uuid');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
